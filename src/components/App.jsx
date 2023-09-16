853c3774a3c82a8f42e253f000e8a0ca052d108e
Ursx2204 <127949449+Yurii2276@users.noreply.github.com>
2023-09-16 22:01:53 +0300

    2 finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ export const App = () => {
         stats={user.stats}
       />
 
-      {/* <Statistics title="Upload stats!!!!!!" stats={data} /> */}
+      <Statistics title="Upload stats" stats={data} />
 
       {/* <FriendList friends={friends}/> */}
 
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,18 +1,38 @@
+import css from './Statistic.module.css';
+
+import PropTypes from 'prop-types';
+
+function generateRandomColor() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const color = `rgb(${red}, ${green}, ${blue})`;
+  return color;
+}
+
 export default function Statistics({ title, stats }) {
   return (
-    <div>
-      <section className="statistics">
-        {title && <h2 className="title">{title}</h2>}
+    <div className={css.container}>
+      <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
 
-        <ul className="stat-list">
+        <ul className={css.statList}>
           {stats.map((element, index) => (
-            <li key={index} className="item">
-              <span className="label">{element.label}</span>
-              <span className="percentage">{element.percentage}%</span>
+            <li key={index} className={css.item} style={{ backgroundColor: generateRandomColor() }}>
+              <span className={css.label}>{element.label}</span>
+              <span className={css.percentage}>{element.percentage}%</span>
             </li>
           ))}
         </ul>
       </section>
     </div>
   );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  })
 }
